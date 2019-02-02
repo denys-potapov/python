29e911a5e14dca756e1091d5bac7f1a0406dde8d
denys <denys.potapov@gmail.com>
2019-02-02 14:28:09 +0200

    move to

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ DIRECTIONS = {
 
 class Game():
     id = ''
+    hive_pos = None
 
     def cell(self, pos, dir = False):
         x, y = pos
@@ -25,17 +26,15 @@ class Game():
 
         return self.map['cells'][y][x]
 
-    def order(self, ant, act, dirs, probs=None):
-        dir = random.choice(dirs)
-        if probs is not None:
-            choices = []
-            for i in len(probs):
-                choices += [i] * probs[i]
-            dir = dirs[random.choice(choices)]
+    # dirs = [(dir, probability)]
+    def order(self, ant, act, dirs):
+        choices = []
+        for d, p in dirs:
+            choices += [d] * p
 
         self.orders[ant['id']] = {
             'act': act,
-            'dir': dir
+            'dir': random.choice(choices)
         }
 
         return True
@@ -47,8 +46,14 @@ class Game():
             ant['id'] = i
             ant['pos'] = (ant['x'], ant['y'])
             self.free_ants.append(ant)
-
         self.map = hive['map']
+
+        # load only on first turn
+        if self.hive_pos is None:
+            for y, row in enumerate(self.map['cells']):
+                for x, cell in enumerate(row):
+                    if cell.get('hive') == self.id:
+                        self.hive_pos = (x, y)
 
     def unload_dir(self, pos):
         for dir in DIRECTIONS.keys():
@@ -66,18 +71,50 @@ class Game():
         food = filter(lambda f: f[1] > 0, food)
         return sorted(food, key = lambda f: f[1])
 
+    def move_dirs(self, pos, target):
+        can_dirs = []
+        dx = target[0] - pos[0]
+        dy = target[1] - pos[1]
+        if dx != 0:
+            can_dirs.append(('left' if dx > 0 else 'right', abs(dx)))
+        if dy != 0:
+            can_dirs.append(('bottom' if dy > 0 else 'up', abs(dy)))
+
+        # only if we can
+        move_dirs = []
+        for (dir, p) in can_dirs:
+            cell = self.cell(pos, dir)
+            if ('food' in cell) or ('ant' in cell):
+                continue
+            if cell.get('hive', self.id) != self.id:
+                continue
+            move_dirs.append((dir, p))
+
+        return move_dirs
+
+
     def do_loaded(self, ant):
         if ant['payload'] == 0:
             return True
         # can unload
         unload_dir = self.unload_dir(ant['pos'])
         if unload_dir:
-            self.order(ant, 'unload', [unload_dir])
+            self.order(ant, 'unload', [(unload_dir, 1)])
             return False
+        
         # can load
         load_dirs = self.load_dirs(ant['pos'])
-        if load_dirs:
+        if (len(load_dirs) > 0) and ant['payload'] < 5:
+            self.order(ant, 'unload', load_dirs)
             return False
+
+        # go to hive
+        move_dirs = self.move_dirs(ant['pos'], self.hive_pos)
+        if len(move_dirs) > 0:
+            self.order(ant, 'unload', move_dirs)
+            return False
+        else:
+            self.order(ant, 'stay', ('left', 1))
 
         return False
 
@@ -89,13 +126,13 @@ class Game():
         self.free_ants = filter(lambda a: self.do_loaded(a), self.free_ants)
 
         # Loop through ants and give orders
-        for ant in self.free_ants:
-            self.order(
-                ant,
-                ACTIONS[random.randint(0, 4)],
-                list(DIRECTIONS.keys())
-            )
-        # json format sample:
+        # for ant in self.free_ants:
+        #     self.order(
+        #         ant,
+        #         ACTIONS[random.randint(0, 4)],
+        #         ['le'
+        #     )
+        # # json format sample:
         # {"1":{"act":"load","dir":"down"},"17":{"act":"load","dir":"up"}}
 
         return self.orders
--- test_run.py
+++ test_run.py
@@ -13,6 +13,7 @@ def def_game():
 def test_game_load_hive():
     game = def_game()
     assert len(game.free_ants) == 2
+    assert game.hive_pos == (0, 0)
 
 
 def test_game_do_turn():
@@ -31,3 +32,8 @@ def test_unload_dir():
 def test_unload_dir():
     game = def_game()
     assert game.load_dirs((1, 1)) == [('left', 5)]
+
+
+def test_move_dirs():
+    game = def_game()
+    assert game.move_dirs((1, 1), (0, 0)) == [('right', 1)]
