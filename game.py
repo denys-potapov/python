9541619950ae04ab45557a64082121dd48edd62f
denys <denys.potapov@gmail.com>
2019-02-02 14:51:54 +0200

    thsi could works

--- game.py
+++ game.py
@@ -49,11 +49,16 @@ class Game():
         self.map = hive['map']
 
         # load only on first turn
-        if self.hive_pos is None:
-            for y, row in enumerate(self.map['cells']):
-                for x, cell in enumerate(row):
-                    if cell.get('hive') == self.id:
-                        self.hive_pos = (x, y)
+        self.food = []
+        for y, row in enumerate(self.map['cells']):
+            for x, cell in enumerate(row):
+                # load hive pos on firts turn
+                if (self.hive_pos is None) and (cell.get('hive') == self.id):
+                    self.hive_pos = (x, y)
+                # fill food
+                f = cell.get('food', 0)
+                if (f > 0) and (len(cell) == 1):
+                    self.food.append(((x, y), f))
 
     def unload_dir(self, pos):
         for dir in DIRECTIONS.keys():
@@ -71,6 +76,9 @@ class Game():
         food = filter(lambda f: f[1] > 0, food)
         return sorted(food, key = lambda f: f[1])
 
+    def dist(self, pos, target):
+        return abs(target[0] - pos[0]) + abs(target[1] - pos[1])
+
     def move_dirs(self, pos, target):
         can_dirs = []
         dx = target[0] - pos[0]
@@ -78,7 +86,7 @@ class Game():
         if dx != 0:
             can_dirs.append(('left' if dx > 0 else 'right', abs(dx)))
         if dy != 0:
-            can_dirs.append(('bottom' if dy > 0 else 'up', abs(dy)))
+            can_dirs.append(('down' if dy > 0 else 'up', abs(dy)))
 
         # only if we can
         move_dirs = []
@@ -92,6 +100,12 @@ class Game():
 
         return move_dirs
 
+    def move_to(self, ant, target):
+        move_dirs = self.move_dirs(ant['pos'], target)
+        if len(move_dirs) > 0:
+            self.order(ant, 'unload', move_dirs)
+        else:
+            self.order(ant, 'stay', ('left', 1))
 
     def do_loaded(self, ant):
         if ant['payload'] == 0:
@@ -105,34 +119,44 @@ class Game():
         # can load
         load_dirs = self.load_dirs(ant['pos'])
         if (len(load_dirs) > 0) and ant['payload'] < 5:
-            self.order(ant, 'unload', load_dirs)
+            self.orderpass(ant, 'unload', load_dirs)
             return False
 
-        # go to hive
-        move_dirs = self.move_dirs(ant['pos'], self.hive_pos)
-        if len(move_dirs) > 0:
-            self.order(ant, 'unload', move_dirs)
-            return False
-        else:
-            self.order(ant, 'stay', ('left', 1))
-
+        # go home
+        self.move_to(ant, self.hive_pos)
         return False
+
+    def do_food(self):
+        self.food.sort(key = lambda f: - self.dist(f[0], self.hive_pos))
+        for pos, _ in self.food:
+            min_ant = None
+            min_d = 1000000
+            min_i = None
+            for i, ant in enumerate(self.free_ants):
+                d = self.dist(pos, ant['pos'])
+                if d < min_d:
+                    min_ant = ant
+                    min_d = d
+                    min_i = i
+
+            if min_ant is None:
+                return
+            self.move_to(ant, pos)
+            del(self.free_ants[min_i])
+        
 
     def do_turn(self, hive):
         self.load_hive(hive)
         self.orders = {}
 
         # loaded ants
-        self.free_ants = filter(lambda a: self.do_loaded(a), self.free_ants)
+        self.free_ants = list(filter(lambda a: self.do_loaded(a), self.free_ants))
 
-        # Loop through ants and give orders
-        # for ant in self.free_ants:
-        #     self.order(
-        #         ant,
-        #         ACTIONS[random.randint(0, 4)],
-        #         ['le'
-        #     )
-        # # json format sample:
-        # {"1":{"act":"load","dir":"down"},"17":{"act":"load","dir":"up"}}
+        # find food
+        self.do_food()
+
+        # stay still
+        for ant in self.free_ants:
+             self.order(ant, 'stay', ('left', 1))
 
         return self.orders
